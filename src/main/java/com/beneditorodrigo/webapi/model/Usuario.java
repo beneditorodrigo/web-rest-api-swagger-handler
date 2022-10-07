12195973a1420df6581d94305891902d7f97beda
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-10-06 23:13:08 -0300

    Criando a entidade Usuario (usando Lombok para métodos de acesso)

---
+++ src/main/java/com/beneditorodrigo/webapi/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.beneditorodrigo.webapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+
+    private Integer id;
+    private String login;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(String login, String password){
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
