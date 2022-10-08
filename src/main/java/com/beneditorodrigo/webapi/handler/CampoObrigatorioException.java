94f7b763c3cee19b6c6abbf997d0e5a192435fa6
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-10-08 00:52:21 -0300

    Tratando/manipulando exceções com Exception Handler

---
+++ src/main/java/com/beneditorodrigo/webapi/handler/CampoObrigatorioException.java
@@ -0,0 +1,7 @@
+package com.beneditorodrigo.webapi.handler;
+
+public class CampoObrigatorioException extends BusinessException{
+    public CampoObrigatorioException(String campo) {
+        super("O campo %s é obrigatório!", campo);
+    }
+}
---
+++ src/main/java/com/beneditorodrigo/webapi/handler/ResponseError.java
@@ -0,0 +1,15 @@
+package com.beneditorodrigo.webapi.handler;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ResponseError {
+    private Date timestamp = new Date();
+    private String status = "error";
+    private int statusCode = 400;
+    private String error;
+}
